69c1aeda71873e6c7048f013592b04aa3f1a364d
zhenyu lin <zhen3363494168@gmail.com>
2026-04-25 16:15:17 +0800

    Add extension config

---
+++ extension/config.js
@@ -0,0 +1,13 @@
+window.MI_CONFIG = {
+    sponsor: {
+        enabled: true,
+        label: '',
+        href: 'https://github.com/your-repo/sponsor',
+        title: '',
+        useI18nLabel: true,
+        newTab: true
+    },
+    advanced: {
+        fetchAccountInfo: true
+    }
+};
